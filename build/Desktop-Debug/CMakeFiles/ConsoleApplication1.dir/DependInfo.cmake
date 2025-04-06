526562738465475fdeecf739865b81f2a6649b5a
pedro <pedro.areias@tecnico.ulisboa.pt>
2025-04-06 20:37:39 +0100

    New

---
+++ build/Desktop-Debug/CMakeFiles/ConsoleApplication1.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pedro/SimPlas/ConsoleApplication1/entity.cpp" "CMakeFiles/ConsoleApplication1.dir/entity.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/entity.cpp.o.d"
+  "/home/pedro/SimPlas/ConsoleApplication1/main.cpp" "CMakeFiles/ConsoleApplication1.dir/main.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/main.cpp.o.d"
+  "/home/pedro/SimPlas/ConsoleApplication1/relationmatrix.cpp" "CMakeFiles/ConsoleApplication1.dir/relationmatrix.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/relationmatrix.cpp.o.d"
+  "/home/pedro/SimPlas/ConsoleApplication1/relationonetomany.cpp" "CMakeFiles/ConsoleApplication1.dir/relationonetomany.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/relationonetomany.cpp.o.d"
+  "/home/pedro/SimPlas/ConsoleApplication1/relmanytomany.cpp" "CMakeFiles/ConsoleApplication1.dir/relmanytomany.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/relmanytomany.cpp.o.d"
+  "/home/pedro/SimPlas/ConsoleApplication1/test.cpp" "CMakeFiles/ConsoleApplication1.dir/test.cpp.o" "gcc" "CMakeFiles/ConsoleApplication1.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Desktop-Debug/CMakeFiles/ConsoleApplication1.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConsoleApplication1.dir/entity.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/entity.cpp.o.d"
+  "CMakeFiles/ConsoleApplication1.dir/main.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/main.cpp.o.d"
+  "CMakeFiles/ConsoleApplication1.dir/relationmatrix.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/relationmatrix.cpp.o.d"
+  "CMakeFiles/ConsoleApplication1.dir/relationonetomany.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/relationonetomany.cpp.o.d"
+  "CMakeFiles/ConsoleApplication1.dir/relmanytomany.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/relmanytomany.cpp.o.d"
+  "CMakeFiles/ConsoleApplication1.dir/test.cpp.o"
+  "CMakeFiles/ConsoleApplication1.dir/test.cpp.o.d"
+  "ConsoleApplication1"
+  "ConsoleApplication1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConsoleApplication1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
